8574d9054776dcb01dfae50630db9c22c428f54b
basti122303 <basti@unix-solution.de>
2014-10-29 12:43:18 +0100

    Update zonedelete.php
    Add routine to delete zonefile.

--- php/src/zonedelete.php
+++ php/src/zonedelete.php
@@ -9,7 +9,9 @@ if(is_admin()) {
 					 "WHERE id = " . $_GET['i']
 				   );
 		is_error($res);
-		$smarty->assign("zone", current($res->fetchRow(0)));
+		// get the name of the zone, to delete zonefile
+		$zonename = current($res->fetchRow(0));
+		$smarty->assign("zone", $zonename);
 		$res = $dbconnect->query("DELETE FROM zones " .
 					 "WHERE id = " . $_GET['i']
 				   );
@@ -23,6 +25,16 @@ if(is_admin()) {
 					 "WHERE zone = " . $_GET['i']
 				   );
 		is_error($res);
+		// delete zonefile
+		// FIXME: zonefile is always delete, even rights are 600 and file is ownwd by root
+		if (!unlink($_CONF['path'] . preg_replace("/\.$/", "",$zonename))) {
+		    $smarty->assign("pagetitle", "Ooops!");
+		    $smarty->assign("reason", reason("filenotdelete"));
+		    $smarty->assign("template", "accessdenied.tpl");
+		    $smarty->assign("help", help("accessdenied"));
+		    $smarty->assign("menu_button", menu_buttons());
+		    $smarty->display("main.tpl");
+		    die();
 	}
 	else {  // Bad input from user.
 		$smarty->assign("pagetitle", "Ooops!");
